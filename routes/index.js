e66bba4bbbdcb5b39d9829c8fb154a0feddafef6
Jeremy <jkaffrey@gmail.com>
2016-09-06 11:32:23 -0600

    More setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Noooo' });
-});
+module.exports = function(router, io) {
 
-module.exports = router;
+  router.get('/', function(req, res, next) {
+
+    res.render('index', { title: 'Noooo' });
+  });
+
+  router.get('/code', function(req, res, next) {
+
+    var uniqueId = Math.random().toString(36).substring(7);
+    res.redirect('/code/' + uniqueId);
+  });
+
+  router.get('/code/:id', function(req, res, next) {
+
+    res.render('codeView');
+  });
+};
+
+function findClientsSocket(io, roomId, namespace) {
+  
+  var res = [],
+  ns = io.of(namespace || '/');
+
+  if (ns) {
+    for (var id in ns.connected) {
+      if(roomId) {
+        var index = ns.connected[id].rooms.indexOf(roomId) ;
+        if(index !== -1) {
+          res.push(ns.connected[id]);
+        }
+      }
+      else {
+        res.push(ns.connected[id]);
+      }
+    }
+  }
+  return res;
+}
