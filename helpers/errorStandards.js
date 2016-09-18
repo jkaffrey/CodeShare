75521415ec213834a18fb69b5f6fc1ff6eab2b40
Jeremy <jkaffrey@gmail.com>
2016-09-18 14:41:06 -0600

    Validity

--- helpers/errorStandards.js
+++ helpers/errorStandards.js
@@ -4,5 +4,5 @@ module.exports = {
 
   invalidEmail: 'The email that was entered cannot be found.',
   invalidPassword: 'The password that you entered is invalid.',
-  unauthorizedRequest: 'You do not have permission to view this page. This may be because your session has timed out or you are not logged in.'
+  unauthorizedRequest: 'You do not have permission to view this page. This may be because your session has timed out or you are not logged in.',
 };
--- public/javascripts/validations.js
+++ public/javascripts/validations.js
@@ -1,12 +1,21 @@
 'use strict';
 
+// const errors = require('../../helpers/errorStandards');
+
+/* Validation Errors */
+const v_invalid_email = 'The email you have entered is not valid, please correct this before submitting.',
+      v_invalid_name = 'The name you have entered is not valid, please enter your first and last name.',
+      v_invalid_password = 'The password you have entered is not valid, passwords must have atleast one upper and lower case letter and a number.',
+      v_invalid_match = 'The passwords that you have entered do not match',
+      v_invalid_sa = 'You must answer the security question';
+
 const email = document.getElementById('email'),
       f_name = document.getElementById('name'),
       password = document.getElementById('password'),
       p_confirm = document.getElementById('confirm'),
       sa = document.getElementById('securityAnswer');
 
-/* Email Validation */
+/* Name Validation */
 
 f_name.addEventListener('keyup', function(event) {
 
@@ -30,6 +39,7 @@ f_name.addEventListener('focusout', function(event) {
 
 email.addEventListener('keyup', function(event) {
 
+  console.log(email.validity);
   if (!email.validity.patternMismatch) {
 
     document.getElementById('errorEmail').style.display = 'none';
@@ -79,12 +89,13 @@ p_confirm.addEventListener('keyup', function(event) {
 });
 
 p_confirm.addEventListener('focusout', function(event) {
-  console.log(p_confirm.value, password.value);
   if (p_confirm.value !== password.value) {
 
+    p_confirm.setCustomValidity('Passwords must match.');
     document.getElementById('errorConfirm').style.display = 'block';
   } else {
 
+    p_confirm.setCustomValidity('');
     document.getElementById('errorConfirm').style.display = 'none';
   }
 });
